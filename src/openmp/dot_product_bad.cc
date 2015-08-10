87e87bbe71ebe90791078886f6930bf1212928d1
Krishna Kumar <kks32@gmx.com>
2015-08-11 01:49:18 +0200

    distributed programming: Introduction

---
+++ src/openmp/dot_product_bad.cc
@@ -0,0 +1,20 @@
+#include <omp.h>
+
+#include <cstdio>
+#include <cstdlib>
+const unsigned size = 1000000;
+const unsigned chunk = 10;
+int main(int argc, char* argv[]) {
+  int i;
+  float a[size], b[size], sum = 0.0;
+  // Some initializations
+  for (i = 0; i < size; ++i) a[i] = b[i] = 1.0;
+
+// parallel dot product
+#pragma omp parallel for default(shared) private(i) \
+  schedule(dynamic, chunk)
+  for (i = 0; i < size; ++i) sum += (a[i] * b[i]);
+
+  printf("Sum = %f\n", sum);
+}
+
